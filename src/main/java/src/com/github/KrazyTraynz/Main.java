15eaeecc4c2f498948de076c8dcddcc09231dc34
KrazyTraynz <carter.milch@yahoo.com>
2013-11-30 00:44:19 -0500

    Added plugin structure for real

---
+++ src/main/java/src/com/github/KrazyTraynz/Main.java
@@ -0,0 +1,14 @@
+package com.github.KrazyTraynz;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+}
